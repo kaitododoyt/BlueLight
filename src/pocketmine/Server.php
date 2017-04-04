bd9b109375dd61ac0ab42d672e9eab4a18d0d7e1
caspervanneck <wesleyvanneck1012@gmail.com>
2017-04-04 16:01:51 +0200

    Some cleanup

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1455,8 +1455,8 @@ class Server{
 				"HungerTimer" => 80,
 				"RideableEntity" => false,
 				"CleanEntity" => false,
-			"allowInventoryCheats" => false,		
-]);
+				"allowInventoryCheats" => false,	
+                             ]);
 
 			$this->logger->info("Loading server properties...");
 			$this->properties = new Config($this->dataPath . "server.properties", Config::PROPERTIES, [
@@ -1512,7 +1512,7 @@ class Server{
 			$this->golemspawn = $this->getProperty("GolemSpawn", false);
 			$this->rideableentity = $this->getProperty("RideableEntity", false);
 			$this->cleanentity = $this->getProperty("CleanEntity", false)
-		$this->allowInventoryCheats = $this->getAdvancedProperty("inventory.allow-cheats", false);
+			$this->allowInventoryCheats = $this->getAdvancedProperty("inventory.allow-cheats", false);
 
 			if($this->crashdump){
 				if(!file_exists($dataPath . "crashdumps/")){
